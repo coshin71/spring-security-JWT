bac2bbe30195090a5a2c598c3c75e8dc673e03fe
coshin71 <mincolong@naver.com>
2022-04-03 12:12:45 +0900

    Set up security config

---
+++ src/main/java/hello/springsecurityJWT/User.java
@@ -0,0 +1,19 @@
+package hello.springsecurityJWT;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+}
